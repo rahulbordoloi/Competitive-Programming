72770f728fc5fca8ffed1d85318b83f4b9b68898
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-05-24 15:47:06 +0530

    Ugly Number Problem

--- ugly_no.py
+++ ugly_no.py
@@ -1,4 +1,4 @@
-"""Ugly Numbers
+"""Ugly Numbers 
 Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11 ugly numbers. 
 By convention, 1 is included.
 Given a number n, the task is to find n’th Ugly number."""
